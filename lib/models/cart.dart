927cb310a63daabd43263e64296de03e2dfaff70
eeaaae <khirsagarp88@gmailcom>
2022-09-08 09:33:07 -0700

    Day 27 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   // single tone --> not required because store.dart has something new
@@ -25,13 +27,18 @@ class CartModel {
   // Get total price
   num get totalPrice => items.fold(0, (total, current) => total + current.price);
 
-  // Add Item
-  void add(Item item) {
-    _itemIds.add(item.id);
-  }
-
   // Remove Item
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store!.cart._itemIds.add(item.id);
   }
 }
